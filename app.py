5ddb030b08cddf9f4a781a704bcc34df03e4f1ff
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-02-12 09:47:06 -0200

    Add new graphic.

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask('d3-app')
 
 @app.route('/') 
 def index():    
-    return render_template('map.html')
+    return render_template('maps.html')
+
+@app.route('/graphic/')
+def graphic():
+    return render_template('graphic2.html')
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)    
