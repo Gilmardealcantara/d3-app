2e1d11d19dd0f75c2ace483d13bf1c8d5ac0464a
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-02-19 12:53:44 -0200

    Read of simple json

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ app = Flask('d3-app')
 
 @app.route('/') 
 def index():    
-    return render_template('index.html')
-
-@app.route('/stacked') 
-def stacked():    
     return render_template('maps.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)    
--- static/js/stacked.js
+++ static/js/stacked.js
@@ -6,39 +6,27 @@ var stackedGraphic = ( function (){
     } 
 
     function init(){
-        // sample data array
-        var sample_data = [
-        {"year": 2002, "name":"alpha", "value": 20},
-        {"year": 2002, "name":"alpha", "value": 30},
-        {"year": 2002, "name":"alpha", "value": 60},
-        {"year": 2002, "name":"beta", "value": 40},
-        {"year": 2002, "name":"beta", "value": 60},
-        {"year": 2002, "name":"beta", "value": 10},
-        {"year": 2003, "name":"gamma", "value": 10},
-        {"year": 2003, "name":"gamma", "value": 40},
-        {"year": 2003, "name":"alpha", "value": 20},
-        {"year": 2003, "name":"alpha", "value": 30},
-        {"year": 2006, "name":"alpha", "value": 60},          
-        ]
-        // instantiate d3plus
-        var visualization = d3plus.viz()
-        .container("#viz")  // container DIV to hold the visualization
-        .data(sample_data)  // data to use with the visualization
-        .type("stacked")    // visualization type
-        .id("name")         // key for which our data is unique on
-        .text("name")       // key to use for display text
-        .y("value")         // key to use for y-axis
-        .x("year")          // key to use for x-axis
-        .time("year")       // key to use for time
-        .active(false)
-        .temp(false)
-        .total(false)
-        .id({"grouping": true})
-        .shape("area")
-        .depth(3)    
-        .draw()             // finally, draw the visualization!
+        var width = 960,
+            height = 500;
+
+        var color = d3.scale.category20();
+
+        var stack = d3.layout.stack()
+            .offset("wiggle")
+            .values(function(d) { return d.values; });
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        d3.json("/static/js/miserables.json", function(error, graph) {
+          if (error) throw error;
+                    
+        });    
     }
 
 })();
+
+
 
 stackedGraphic.init();
