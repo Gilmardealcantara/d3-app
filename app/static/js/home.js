16665a0b40a7c04aad4532a56a637706aa5af7e0
gilmardealcantara <gilmardealcantara@gmail.com>
2016-06-06 17:40:06 -0300

    Change layout and add treemap.

---
+++ app/static/js/home.js
@@ -0,0 +1,45 @@
+window.onload = function(){
+	var objeto = document.getElementById('objeto');
+	document.getElementById("fade").onclick = function(){
+    	
+    	if (document.getElementById("fade").value == "active"){
+    		document.getElementById("fade").value = "deactive";
+    		fadeOut(objeto,1);
+    	}else{
+			document.getElementById("fade").value = "active";
+    		fadeIn(objeto,1);
+    	}
+	}
+}
+
+function fadeIn(element,time){
+	processa(element,time,0,100);
+}
+
+function fadeOut(element,time){
+	processa(element,time,100,0);
+}
+
+function processa(element,time,initial,end){
+	if(initial == 0){
+         	increment = 2;
+         	element.style.display = "block";
+	}else {
+         	increment = -2;
+	}
+
+	opc = initial;
+
+	intervalo = setInterval(function(){
+         	if((opc == end)){
+                      if(end == 0){
+                                     	element.style.display = "none";
+                      }
+                      clearInterval(intervalo);
+         	}else {
+                      opc += increment;
+                      element.style.opacity = opc/100;
+                      element.style.filter = "alpha(opacity="+opc+")";
+         	}
+	},time * 10);
+}
--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from app import app
 
+mod = Blueprint('app', __name__,
+                template_folder='templates',)
+
+
+@app.route('/home')
 @app.route('/') 
 def index():    
     return render_template('index.html')
 
+@app.route('/stacked')
+def stacked():
+    return render_template('stacked.html')
+
+@app.route('/treemap')
+def treemap():
+    return render_template('treemap.html')
